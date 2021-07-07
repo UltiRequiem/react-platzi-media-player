e520c5cdaea96926763fd3e0d8b7e70a9b969c21
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-07-07 13:53:56 -0500

    :wrench: Add Configuration Files

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    es6: true
+  },
+  ignorePatterns: ['node_modules'],
+  extends: [
+    'airbnb-base',
+    'prettier',
+    'eslint:recommended',
+    'plugin:import/react',
+    'plugin:react/recommended'
+  ],
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx']
+      }
+    }
+  },
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module'
+  },
+  rules: {
+    quotes: ['warn', 'single', 'avoid-escape'],
+    'no-plusplus': 'off',
+    'on-var': 'off',
+    'max-classes-per-file': 'off',
+    'no-console': 'off',
+    'react/react-in-jsx-scope': 'off'
+  }
+};
