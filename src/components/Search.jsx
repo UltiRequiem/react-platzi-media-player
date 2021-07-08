9ab200cc458ecaad853dec90fd5cec5dc6ce84bc
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-07-08 09:38:03 -0500

    :art: Update

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,10 +4,11 @@ import '@styles/components/Search.scss';
 const mainTitleText = 'What do you wann watch today?';
 const mainInputPlaceholder = 'Search';
 
-// eslint-disable-next-line react/display-name
-export default () => (
+const Search = () => (
   <section className="main">
     <h2 className="main__title">{mainTitleText}</h2>
     <input type="text" className="input" placeholder={mainInputPlaceholder} />
   </section>
 );
+
+export default Search;
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
+// Proyect Components
 import Header from '@components/Header';
 import Search from '@components/Search';
 import Categories from '@components/Categories';
@@ -7,29 +8,43 @@ import Carousel from '@components/Carousel';
 import CarouselItem from '@components/CarouselItem';
 import Footer from '@components/Footer';
 
+// Proyect Style
 import '@styles/App.scss';
 
+// Items
 const CAROUSELS = [
   { topic: 'Movies', items: [1, 2, 3] },
   { topic: 'Anime', items: [1, 2, 3, 4] },
   { topic: 'Others', items: [1, 2, 3, 4, 5, 6] }
 ];
 
-const App = () => (
-  <div className="App">
-    <Header />
-    <Search />
-    {CAROUSELS.map((carousel, index) => (
-      <Categories title={carousel.topic} key={index}>
-        <Carousel>
-          {carousel.items.map((_item, i) => (
-            <CarouselItem key={i} />
-          ))}
-        </Carousel>
-      </Categories>
-    ))}
-    <Footer />
-  </div>
-);
+const App = () => {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/initalState')
+      .then((response) => response.json())
+      .then((data) => setVideos(data));
+  }, []);
+
+  console.log(videos)
+
+  return (
+    <div className="App">
+      <Header />
+      <Search />
+      {CAROUSELS.map((carousel, index) => (
+        <Categories title={carousel.topic} key={index}>
+          <Carousel>
+            {carousel.items.map((_item, i) => (
+              <CarouselItem key={i} />
+            ))}
+          </Carousel>
+        </Categories>
+      ))}
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
